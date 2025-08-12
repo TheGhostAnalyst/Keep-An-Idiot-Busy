97ce1c3f2d99cd36720d0c8cc30ced2612f1ae99
The Ghost Analyst <habibshittu8@gmail.com>
2025-08-12 18:44:32 -0400

    KeepAnIdiotBusy.py

---
+++ KeepAnIdiotBusy.py
@@ -0,0 +1,14 @@
+import pyinputplus as pyip
+import time
+
+while True:
+    response = pyip.inputYesNo(prompt='Do you want to know how to keep an idiot busy for hours: ')
+    if response == 'no':
+        print('Thanks for your time..')
+        break
+    else:
+        print("Great! Here's a fun task for you: Count to 10.. slowly!")
+        for i in range(1, 11):
+            print(f"Counting... {i}")
+            time.sleep(1)  # wait 1 second each count to drag it out
+        print("Finally i kept you busy for a while! sorry for falling :).")
